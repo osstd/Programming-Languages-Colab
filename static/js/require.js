ee2ca38eab2d15c22eaa5f0b8f0e8097514f96ba
osstd <omarelshemy94@gmail.com>
2024-06-05 16:14:52 -0400

    add graphs and promises handling

--- static/js/require.js
+++ static/js/require.js
@@ -1,125 +1,161 @@
-// Get the CSV file
-fetch("static/assets/csv/pivoted_data.csv")
-  .then((response) => response.text())
-  .then((data) => {
-    const pdf = Plotly.d3.csv.parse(data);
-    console.log(pdf[0]);
-    // Create line plot
-    const line = {
-      data: [
-        {
-          x: pdf.map((row) => row.DATE),
-          y: pdf.map((row) => row.java),
-          name: "Java",
-          type: "line",
-        },
-        {
-          x: pdf.map((row) => row.DATE),
-          y: pdf.map((row) => row.python),
-          name: "Python",
-          type: "line",
-        },
-      ],
-      layout: {
-        title: {
-          text: "Posts Over Time",
-          font: {
-            size: 20,
-          },
-        },
-        xaxis: {
-          title: "Date",
-          showgrid: false,
-          tickfont: {
-            size: 12,
-          },
-        },
-        yaxis: {
-          title: "Number of Posts",
-          showgrid: false,
-          tickfont: {
-            size: 12,
-          },
-          range: [0, 35000],
-        },
-        font: {
-          family: "Verdana",
-        },
-        autosize: true,
-      },
-      config: {
-        responsive: true,
-      },
-    };
-    // Create scatter plot
-    const columns = Object.keys(pdf[0]).filter((col) => col !== "DATE");
+async function fetchCSVData(url) {
+  const response = await fetch(url);
+  const data = await response.text();
+  return data;
+}
 
-    const scatter = {
-      data: [],
-      layout: {
-        autosize: true,
-        title: {
-          text: "Posts Over Time",
-          font: {
-            size: 20,
-          },
-        },
-        xaxis: {
-          title: "Date",
-          showgrid: false,
-          tickfont: {
-            size: 12,
-          },
-        },
-        yaxis: {
-          title: "Number of Posts",
-          showgrid: false,
-          tickfont: {
-            size: 12,
-          },
-          range: [0, 35000],
-        },
-        legend: {
-          orientation: "h",
-          yanchor: "top",
-          y: -0.2,
-          xanchor: "center",
-          x: 0.5,
-          font: {
-            size: 10,
-          },
-        },
-        margin: {
-          l: 50,
-          r: 20,
-          t: 40,
-          b: 80,
-        },
-        plot_bgcolor: "rgba(0, 0, 0, 0)",
-        paper_bgcolor: "rgba(0, 0, 0, 0)",
-        font: {
-          family: "Verdana",
-        },
-      },
-      config: {
-        responsive: true,
-      },
-    };
+async function plotLine(data, title, num) {
+  const pdf = Plotly.d3.csv.parse(data);
 
-    // Create traces for each column
-    columns.map((column) => {
-      scatter.data.push({
+  // Create line plot
+  const line = {
+    data: [
+      {
         x: pdf.map((row) => row.DATE),
-        y: pdf.map((row) => row[column]),
-        mode: "lines",
-        name: column,
-        type: "scatter",
-      });
+        y: pdf.map((row) => row.java),
+        name: "Java",
+        type: "line",
+      },
+      {
+        x: pdf.map((row) => row.DATE),
+        y: pdf.map((row) => row.python),
+        name: "Python",
+        type: "line",
+      },
+    ],
+    layout: {
+      title: {
+        text: `${title}`,
+        font: {
+          size: 20,
+        },
+      },
+      xaxis: {
+        title: "Date",
+        showgrid: false,
+        tickfont: {
+          size: 12,
+        },
+      },
+      yaxis: {
+        title: "Number of Posts",
+        showgrid: false,
+        tickfont: {
+          size: 12,
+        },
+        range: [0, 35000],
+      },
+      font: {
+        family: "Verdana",
+      },
+      autosize: true,
+    },
+    config: {
+      responsive: true,
+    },
+  };
+
+  const lineElement = document.getElementById(`chart${num}`);
+
+  Plotly.plot(lineElement, line.data, line.layout, line.config);
+}
+
+async function plotScatter(data, title, num) {
+  const pdf = Plotly.d3.csv.parse(data);
+
+  // Create scatter plot
+  const columns = Object.keys(pdf[0]).filter((col) => col !== "DATE");
+
+  const scatter = {
+    data: [],
+    layout: {
+      autosize: true,
+      title: {
+        text: `${title}`,
+        font: {
+          size: 20,
+        },
+      },
+      xaxis: {
+        title: "Date",
+        showgrid: false,
+        tickfont: {
+          size: 12,
+        },
+      },
+      yaxis: {
+        title: "Number of Posts",
+        showgrid: false,
+        tickfont: {
+          size: 12,
+        },
+        range: [0, 35000],
+      },
+      legend: {
+        orientation: "h",
+        yanchor: "top",
+        y: -0.2,
+        xanchor: "center",
+        x: 0.5,
+        font: {
+          size: 10,
+        },
+      },
+      margin: {
+        l: 50,
+        r: 20,
+        t: 40,
+        b: 80,
+      },
+      plot_bgcolor: "rgba(0, 0, 0, 0)",
+      paper_bgcolor: "rgba(0, 0, 0, 0)",
+      font: {
+        family: "Verdana",
+      },
+    },
+    config: {
+      responsive: true,
+    },
+  };
+
+  // Create traces for each column
+  columns.map((column) => {
+    scatter.data.push({
+      x: pdf.map((row) => row.DATE),
+      y: pdf.map((row) => row[column]),
+      mode: "lines",
+      name: column,
+      type: "scatter",
     });
-
-    const lineElement = document.getElementById("chart1");
-    const scatterElement = document.getElementById("chart2");
-
-    Plotly.plot(lineElement, line.data, line.layout, line.config);
-    Plotly.plot(scatterElement, scatter.data, scatter.layout, scatter.config);
   });
+
+  const scatterElement = document.getElementById(`chart${num}`);
+
+  Plotly.plot(scatterElement, scatter.data, scatter.layout, scatter.config);
+}
+
+async function main() {
+  const startTime = performance.now();
+  try {
+    const pivotedData = await fetchCSVData(
+      "static/assets/csv/pivoted_data.csv"
+    );
+    const rompdfw6Data = await fetchCSVData("static/assets/csv/rompdfw6.csv");
+    const rompdfw12Data = await fetchCSVData("static/assets/csv/rompdfw12.csv");
+
+    // Run plotting functions concurrently
+    await Promise.all([
+      plotLine(pivotedData, "Posts Over Time", 1),
+      plotScatter(pivotedData, "Posts Over Time", 2),
+      plotScatter(rompdfw6Data, "Posts Over Time Smooth", 3),
+      plotScatter(rompdfw12Data, "Posts Over Time Smooth", 4),
+    ]);
+  } catch (error) {
+    console.error("Error loading or plotting data:", error);
+  } finally {
+    const endTime = performance.now();
+    console.log(`Execution time: ${endTime - startTime}ms`);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", main);
